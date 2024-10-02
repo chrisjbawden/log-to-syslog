92e22e8106a703f8910fe03c2f7b4adccaa1fb0d
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2024-10-03 09:02:13 +1000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sleep 1
 echo " "
-
+echo " "
 echo "installing plugin ... "
-echo" "
+echo " "
 
 bin/logstash-plugin install logstash-output-syslog
 
@@ -14,6 +14,9 @@ echo "config stuff ..."
 echo " "
 
 curl https://raw.githubusercontent.com/chrisjbawden/log-to-syslog/refs/heads/main/config/logstash.conf -o /usr/share/logstash/pipeline/logstash.conf 
+
+echo " "
+
 echo "xpack.monitoring.enabled: false" | tee -a /usr/share/logstash/config/logstash.yml
 
 sleep 1
