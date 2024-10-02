484465554e92fc7a2274bebaf4c1303f5cb5cb33
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2024-10-03 08:11:34 +1000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,3 +6,7 @@ bin/logstash-plugin install logstash-output-syslog
 sleep 1
 
 curl https://raw.githubusercontent.com/chrisjbawden/log-to-syslog/refs/heads/main/config/logstash.conf -o /usr/share/logstash/pipeline/logstash.conf 
+
+sleep 1
+
+bin/logstash
